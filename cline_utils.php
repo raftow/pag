9f9091323e36d79c7f96e06b64830a04baa9cbcb
raftow <rboubaker@tvtc.gov.sa>
2025-09-19 16:30:57 +0300

    work on crm-v4

--- cline_utils.php
+++ cline_utils.php
@@ -155,16 +155,27 @@ class ClineUtils
         if($php_generation_folder!="no-gen")
         {
             $generated_fileName = $php_generation_folder . $dir_sep . $fileName;
-            AfwFileSystem::write($generated_fileName, $php);
-            $root_module_path = $root_www_path. $dir_sep . $module_code . $dir_sep . $subFolder;
-            $destination_fileName = $root_module_path . $dir_sep . $fileName;
-            $command_lines_arr[] = hzm_format_command_line("info", "php file $fileName has been generated under $php_generation_folder \n");
-            $command_lines_arr[] = hzm_format_command_line("info", "  to install the file :");
-            $command_lines_arr[] = hzm_format_command_line("info", "  if the file is not new use your merge tool $merge_tool and do the following command to merge manually : ");
-            $command_lines_arr[] = hzm_format_command_line("help", "  $merge_tool $generated_fileName $destination_fileName <br>\n");
-            $command_lines_arr[] = hzm_format_command_line("info", "  if the file not do the following command line manually : ");
-            $command_lines_arr[] = hzm_format_command_line("help-mv", "  $mv_command $generated_fileName $root_module_path <br>\n");
-            $mv_command_line = "$mv_command $generated_fileName $root_module_path";
+            try{
+                AfwFileSystem::write($generated_fileName, $php);
+                
+            }
+            catch(Exception $e)
+            {
+                $command_lines_arr[] = hzm_format_command_line("error", "failed to write php file $generated_fileName\n");
+            }
+            finally
+            {
+                $root_module_path = $root_www_path. $dir_sep . $module_code . $dir_sep . $subFolder;
+                $destination_fileName = $root_module_path . $dir_sep . $fileName;
+                $command_lines_arr[] = hzm_format_command_line("info", "php file $fileName has been generated under $php_generation_folder \n");
+                $command_lines_arr[] = hzm_format_command_line("info", "  to install the file :");
+                $command_lines_arr[] = hzm_format_command_line("info", "  if the file is not new use your merge tool $merge_tool and do the following command to merge manually : ");
+                $command_lines_arr[] = hzm_format_command_line("help", "  $merge_tool $generated_fileName $destination_fileName <br>\n");
+                $command_lines_arr[] = hzm_format_command_line("info", "  if the file not do the following command line manually : ");
+                $command_lines_arr[] = hzm_format_command_line("help-mv", "  $mv_command $generated_fileName $root_module_path <br>\n");
+                $mv_command_line = "$mv_command $generated_fileName $root_module_path";
+            }
+            
         }
         else
         {
