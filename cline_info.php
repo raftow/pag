d85e2df30241f9932f3c196351978a19c51220a3
raftow <rboubaker@tvtc.gov.sa>
2025-10-20 18:03:46 +0300

    work on crm-v4

--- cline_info.php
+++ cline_info.php
@@ -1,5 +1,7 @@
 <?php
     phpinfo();
+    "php_ini_loaded_file=".print php_ini_loaded_file()."<br>\n";
+    "php_ini_scanned_files=<pre>".print_r(php_ini_scanned_files())."</pre><br>\n";
     $command_line_result_arr[] = hzm_attribute_command_line("info", "oven", "end", "", "en", "log");
 
     // ex  (explicit if not same code) 
