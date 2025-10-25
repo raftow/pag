81f8ca74385bb43093fb5ab079eaae87c993bc14
rafiq boubaker <rafik.towati@gmail.com>
2025-10-25 16:48:28 +0300

    work on nauss project

--- migrations/migration_00001.php
+++ migrations/migration_00001.php
@@ -1,2 +1,3 @@
 <?php
     // ALTER TABLE `afield` CHANGE `field_name` `field_name` varchar(64) COLLATE 'latin1_swedish_ci' NOT NULL AFTER `shortname_en`;
+    // ALTER TABLE `afield` CHANGE `field_size` `field_size` int NULL AFTER `field_name`;
