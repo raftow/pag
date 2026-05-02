bc9c07d52b26faa0ddf6542ceeb41ac49050d016
rafiq boubaker <rafik.towati@gmail.com>
2026-05-02 22:39:41 +0300

    work on nauss project

--- models/afield.php
+++ models/afield.php
@@ -1564,7 +1564,7 @@ class Afield extends PagObject
         }
 
         // row below is structure of attribute and more ...  (atable, obj)
-        public static function to_afield_att($id_main_sh, $row)
+        public static function to_afield_att($id_main_sh, $row, $attribute)
         {
                 $create_table_if_not_exists = AfwSession::config("create_table_if_not_exists", false);
                 $file_dir_name = dirname(__FILE__);
@@ -1659,9 +1659,9 @@ class Afield extends PagObject
                 if (($afield_att['afield_type_id'] == AfwUmsPagHelper::$afield_type_list) or ($afield_att['afield_type_id'] == AfwUmsPagHelper::$afield_type_mlst)) {
                         list($mdl, $tbl, $mdl_id, $tbl_id, $mdl_new, $tbl_new) = AfwUmsPagHelper::getMyModuleAndAtable($id_main_sh, $row['ANSMODULE'], $row['ANSWER'], false, $create_table_if_not_exists);
                         if (!$mdl_id)
-                                throw new AfwRuntimeException('ANSMODULE=' . $row['ANSMODULE'] . ', doesnt have module id');
+                                throw new AfwRuntimeException("For attribute $attribute ANSMODULE=" . $row['ANSMODULE'] . ', doesnt have module id');
                         if (!$tbl_id)
-                                throw new AfwRuntimeException('ANSMODULE=' . $row['ANSMODULE'] . ', ANSWER=' . $row['ANSWER'] . ' doesnt have table id (create_table_if_not_exists=' . $create_table_if_not_exists . ')');
+                                throw new AfwRuntimeException("For attribute $attribute ANSMODULE=" . $row['ANSMODULE'] . ', ANSWER=' . $row['ANSWER'] . ' doesnt have table id (create_table_if_not_exists=' . $create_table_if_not_exists . ')');
                         $afield_att['answer_module_id'] = $mdl_id;
                         $afield_att['answer_table_id'] = $tbl_id;
                         if (!$afield_att['entity_relation_type_id'])
@@ -3163,7 +3163,7 @@ class Afield extends PagObject
                 return $this->het('atable_id');
         }
 
-       
+
 
         protected function beforeSetAttribute($attribute, $newvalue)
         {
