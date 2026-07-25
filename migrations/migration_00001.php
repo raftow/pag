632c3d4a4e2f887a52a807fc729087d93546f45a
rafiq boubaker <rafik.towati@gmail.com>
2026-07-25 19:52:08 +0300

    some important improvement

--- migrations/migration_00001.php
+++ migrations/migration_00001.php
@@ -15,6 +15,9 @@ SET id = '18',
     sql_field_type='text', 
     oracle_field_type='varchar2(5000)';    
 
+INSERT INTO `afield_type` (`id`, `titre`, `titre_short`, `afield_type_code`, `sql_field_type`, `oracle_field_type`, `is_numeric`, `id_aut`, `date_aut`, `id_mod`, `date_mod`, `id_valid`, `date_valid`, `avail`, `version`, `update_groups_mfk`, `delete_groups_mfk`, `display_groups_mfk`, `sci_id`)
+VALUES ('19', 'كيان صغير', 'كيان صغير', 'sobj', 'text', 'VARCHAR2(5000)', 'N', '1', '2026-05-11 11:30:39', '1', '2026-05-11 11:30:39', NULL, NULL, 'Y', '0', ',', ',', ',', '1');   
+
 DROP TABLE IF EXISTS ttc_pag.synonyms_link;
 
 DROP TABLE IF EXISTS ttc_pag.words;
